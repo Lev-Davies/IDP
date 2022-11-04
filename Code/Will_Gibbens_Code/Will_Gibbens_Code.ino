d56f8d92d3a94e33c88597cf087d7aa8d5103c08
WilliamGibbens <williamgibbens113@gmail.com>
2022-11-04 11:22:33 +0000

    added framework arduino code

---
+++ Code/Will_Gibbens_Code/Will_Gibbens_Code.ino
@@ -0,0 +1,13 @@
+// WG Created: 4/11/22 Modified: 4/11/22
+// Code framework made by Will Gibbens
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
