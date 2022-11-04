fd68dd55a212614f9d5bb87aec94bd3130d230a9
WilliamGibbens <williamgibbens113@gmail.com>
2022-11-04 15:00:22 +0000

    got line sensor code working

--- Code/Will_Gibbens_Code/Will_Gibbens_Code.ino
+++ Code/Will_Gibbens_Code/Will_Gibbens_Code.ino
@@ -1,13 +1,33 @@
 // WG Created: 4/11/22 Modified: 4/11/22
-// Code framework made by Will Gibbens
+// testing the line sensors
+
+const int emitterPin = 13;                // This is the yellow wire for the diode
+const int signalPin =  9;                 // This is the blue wire for the receiver
+int sensorState = 0;                      // variable for reading the sensor state
+float sensorValue = 0;
+int zeros_counter = 0;
+int ones_counter = 0;
 
 
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  pinMode(emitterPin, OUTPUT);
+  pinMode(signalPin, INPUT);
+  digitalWrite(emitterPin, HIGH);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  sensorValue = analogRead(signalPin);
+  sensorState = digitalRead(signalPin);
+  if (sensorState == 1) {
+    ones_counter = ones_counter + 1;
+  }
+  else if (sensorState == 0) {
+    zeros_counter = zeros_counter + 1;
+  }
+  String outputText = "Value: " + String(sensorValue) + " State: " + String(sensorState) + " Whites: " + String(zeros_counter)+ " Blacks: " + String(ones_counter);
+  Serial.println(outputText);
+  delay(100);
 }
