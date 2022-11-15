91eb908b9511d05c7dc545565278fea5cd8baa96
WilliamGibbens <williamgibbens113@gmail.com>
2022-11-15 19:00:37 +0000

    added flashing LED and more comments

--- Software/basic_full_loop.ino
+++ Software/basic_full_loop.ino
@@ -1,6 +1,7 @@
-// WG Created: 4/11/22 Modified: 14/11/22
+// WG Created: 4/11/22 Modified: 15/11/22
 // testing the line sensors, with four line sensors and a Schmitt Trigger
 // Testing the servo
+// Adding flashing light
 
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
@@ -15,9 +16,14 @@ const int sensorLeftPin = 2;
 const int sensorRightPin =  3;
 const int sensorForwardLeftPin = 4;
 const int sensorForwardRightPin = 5;
-const int pingPin = 7;
-const int recievePin = 8;
-const int grabberServoPin = 9;
+const int tunnelPingPin = 6;
+const int tunnelRecievePin = 7;
+const int blockPingPin = 8;
+const int blockRecievePin = 9;
+const int grabberServoPin = 10;
+const int orangeLedPin = 11;
+const int greenLedPin = 12;
+const int redLedPin = 13;
 
 // Define tunnel driving variables
 const int kp = 3;
@@ -28,14 +34,18 @@ unsigned long current_time = 0;
 unsigned long prev_time = 0;
 
 // Define constants
-const int grabber_closed_position = 80;
-const int grabber_open_position = 140;
+const int grabber_closed_position = 80;// degrees
+const int grabber_open_position = 140; // degrees
+const int beacon_flashing_frequency = 2; // Hz
 
 // Initiate variables
-int sensorLeft;  
-int sensorRight;                     // variable for reading the sensor state
-int sensorForwardLeft;  
+int sensorLeft;
+int sensorRight;
+int sensorForwardLeft;
 int sensorForwardRight;
+bool orangeLedState = 0;
+long last_flash_time;
+
 
 Servo grabber;  // create servo object to control a servo
 
@@ -54,12 +64,18 @@ void setup() {
   pinMode(sensorRightPin, INPUT);
   pinMode(sensorForwardLeftPin, INPUT);
   pinMode(sensorForwardRightPin, INPUT);
-  pinMode(pingPin, OUTPUT);
-  pinMode(recievePin, INPUT);
+  pinMode(tunnelPingPin, OUTPUT);
+  pinMode(tunnelRecievePin, INPUT);
+  pinMode(blockPingPin, OUTPUT);
+  pinMode(blockRecievePin, INPUT);
+  pinMode(orangeLedPin, OUTPUT);
+  digitalWrite(orangeLedPin, LOW);
+  pinMode(greenLedPin, OUTPUT);
+  pinMode(redLedPin, OUTPUT);
   AFMS.begin();
   motorLeft->setSpeed(200);
   motorRight->setSpeed(200);
-  grabber.attach(9);  // attaches the servo on pin 9 to the servo object
+  grabber.attach(grabberServoPin);
 }
 
 void loop() {
@@ -70,13 +86,13 @@ void loop() {
   // in inches and centimeters:
   // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
    // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-   digitalWrite(pingPin, LOW);
+   digitalWrite(tunnelPingPin, LOW);
    delayMicroseconds(2);
-   digitalWrite(pingPin, HIGH);
+   digitalWrite(tunnelPingPin, HIGH);
    delayMicroseconds(5);
-   digitalWrite(pingPin, LOW);
+   digitalWrite(tunnelPingPin, LOW);
 
-   pingTime = pulseIn(recievePin, HIGH);
+   pingTime = pulseIn(tunnelRecievePin, HIGH);
 
    mm = distance_in_millimeters(pingTime);
    Serial.print(mm);
@@ -87,72 +103,96 @@ void loop() {
    // whose duration is the time (in microseconds) from the sending of the ping
    // to the reception of its echo off of an object.
 
+  // code for flashing the orange beacon when moving
+  if (millis() > (last_flash_time + 500/beacon_flashing_frequency)){
+    if (orangeLedState == 1){
+      digitalWrite(orangeLedPin, LOW);
+      orangeLedState = 0;
+    }
+    else{
+      digitalWrite(orangeLedPin, HIGH);
+      orangeLedState = 1;
+    }
+    last_flash_time = millis();
+  }
+  // Get data from the line sensors
   sensorLeft = 1 - digitalRead(sensorLeftPin);
   sensorRight = 1 - digitalRead(sensorRightPin);
   sensorForwardLeft = 1 - digitalRead(sensorForwardLeftPin);
   sensorForwardRight = 1 - digitalRead(sensorForwardRightPin);
   String outputText = "Left: " + String(sensorLeft) + " Right: " + String(sensorRight) + " Forward Left: " + String(sensorForwardLeft) + " Forward Right: " + String(sensorForwardRight);
   Serial.println(outputText);
+
   if (sensorLeft == 0 && sensorRight == 0) {
     Serial.println("Line mode");
     motorLeft->setSpeed(220);
     motorRight->setSpeed(220);
     motorLeft->run(FORWARD);
     motorRight->run(FORWARD);
-  } else if (sensorLeft == 0 && sensorRight == 1){
+  }
+  else if (sensorLeft == 0 && sensorRight == 1){
     Serial.println("Line mode");
     motorLeft->setSpeed(20);
     motorRight->setSpeed(220);
     motorLeft->run(FORWARD);
     motorRight->run(FORWARD);
-  } else if (sensorRight == 0 && sensorLeft == 1){
+  }
+  else if (sensorRight == 0 && sensorLeft == 1){
     motorLeft->setSpeed(220);
     motorRight->setSpeed(20);
     motorLeft->run(FORWARD);
     motorRight->run(FORWARD);
-  } else if ((sensorLeft && sensorRight) == 1) {
+  }
+  else if ((sensorLeft && sensorRight) == 1) {
     Serial.println("Ultrasonic mode");
     motorLeft->run(FORWARD);
     motorRight->run(FORWARD);
   
-  if (mm < 180){
-   //controlling motor
- P = target - mm;
-    I = prev_I + P*(current_time-prev_time);
-    error = kp*P + ki*I;
-    prev_time = current_time;
-    prev_I = I;
+    if (mm < 180){
 
-    double speedLeft = med_speed + error;
-    double speedRight = med_speed - error;
+      // Controlling motor using ultrasonic sensor
+      P = target - mm;
+      I = prev_I + P*(current_time-prev_time);
+      error = kp*P + ki*I;
+      prev_time = current_time;
+      prev_I = I;
 
-    if(speedLeft > 255){
-      speedLeft = 255;
-    }
-    if(speedLeft < 0){
-      speedLeft = 0;
-    }
-     if(speedRight > 255){
-      speedRight = 255;
-    }
-    if(speedRight < 0){
-      speedRight = 0;
-    }
+      double speedLeft = med_speed + error;
+      double speedRight = med_speed - error;
 
-    motorLeft->setSpeed(speedLeft);
-    motorRight->setSpeed(speedRight);
-  }else {
-    motorLeft->setSpeed(220);
-    motorRight->setSpeed(220);
-    motorLeft->run(FORWARD);
-    motorRight->run(FORWARD);
-  }
+      if(speedLeft > 255){
+        speedLeft = 255;
+      }
+      else if(speedLeft < 0){
+        speedLeft = 0;
+      }
+      if(speedRight > 255){
+        speedRight = 255;
+      }
+      else if(speedRight < 0){
+        speedRight = 0;
+      }
+
+      motorLeft->setSpeed(speedLeft);
+      motorRight->setSpeed(speedRight);
+    }
+    else {
+      motorLeft->setSpeed(220);
+      motorRight->setSpeed(220);
+      motorLeft->run(FORWARD);
+      motorRight->run(FORWARD);
+    }
   }
   delay(10);
+}
+
+
+
+
 /*
+  // Code for opening and closing the grabber
   grabber.write(grabber_open_position);
   delay(2000);
   grabber.write(grabber_closed_position);
   delay(2000);
   */
-}
