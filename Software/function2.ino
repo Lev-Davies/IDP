008aabd7e97e139ca98ff5c280029430efd7cdf5
ltjldc <80376458+ltjldc@users.noreply.github.com>
2022-11-23 10:15:58 +0000

    Update function2.ino

--- Software/function2.ino
+++ Software/function2.ino
@@ -190,3 +190,86 @@ void tunnel_drive(){
     motorLeft->setSpeed(speedLeft);
     motorRight->setSpeed(speedRight);
 }
+
+
+//block detection from tj - feel free to doubt and make changes as they have not been tested yet
+int foamRecognition() { //return the type of the foam
+  long pingTime, mm;
+  double ave_mm = 0; //average distance
+  int numberOfCylesForAverage = 5;
+  int foamType; // 1 or 0 - high or low
+  int DistanceThreshold = 200; // if the distance from sensor is below this value, it will be recognised as low density - if IR is used simultanously, this can be modified and compared with the result from IR sensor
+
+  for (int cycle = 0; cycle < numberOfCylesForAverage; cycle += 1){
+    digitalWrite(blockPingPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(blockPingPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(blockPingPin, LOW);
+
+    pingTime = pulseIn(blockReceivePin, HIGH);
+
+    mm = distance_in_millimeters(pingTime);
+    ave_mm = ave_mm + mm;
+    //Serial.print(mm);
+    //Serial.print("mm");
+    //Serial.println();
+    delay(10);     
+  }
+  
+  ave_mm = ave_mm / numberOfCylesForAverage;
+
+  if (ave_mm <= DistanceThreshold){
+    foamType = 0; //low density
+    Serial.println("low");
+    Serial.println(ave_mm);
+  } else {
+    foamType = 1; //high density
+    Serial.println("high");
+    Serial.println(ave_mm);
+  }
+  return foamType;
+}
+
+void grab(){ // to be completed!!!! to be integrated with lev's code
+    resetDensity();
+}
+
+void resetDensity(){ //reset when the robot is on the stright line and ready to detect the block
+    density = 1;
+}
+
+void moveBackAndForthAndDetect() { //move back and forth to detect the type of the foam
+    motorLeft->setSpeed(150);
+    motorRight->setSpeed(150);
+    motorLeft->run(BACKWARD);
+    motorRight->run(BACKWARD);
+    for (int i = 0; i < 40; i++){
+      if (foamRecognition() == 0){ //a totay delay of 50 in each foamRocognition()
+        density = 0;
+      }
+      else {
+        density = 1;
+      }
+    }
+    motorLeft->setSpeed(150);
+    motorRight->setSpeed(150);
+    motorLeft->run(FORWARD);
+    motorRight->run(FORWARD);
+    delay(2100); //slightly greater than 2000 to ensure the foam is in front of the robot
+    motorLeft->setSpeed(0);
+    motorRight->setSpeed(0);
+    LEDforFoamType();
+    delay(6000); //The LED should clearly illuminate for >5 seconds while the robot is stationary
+  }
+
+void LEDforFoamType() {
+  if (density == 1){ // high density - red led on
+    digitalWrite(redLedPin, HIGH);
+    digitalWrite(greenLedPin, LOW);
+  }
+  else {
+    digitalWrite(redLedPin, LOW);
+    digitalWrite(greenLedPin, HIGH);
+  }
+}
